2d3e5ddfc85c920cb1236f6f1671b5df366a73b0
Jörg Hoffmann <Joerg.Hoffmann.GitHub@Web.de>
2024-01-01 14:03:18 +0100

    Make AppTest independent from OS CultureInfo

--- FamilyShow.Tests/AppTest.cs
+++ FamilyShow.Tests/AppTest.cs
@@ -1,35 +1,51 @@
-using FamilyShow;
+using System.Globalization;
 
 namespace FamilyShow.Tests;
 
 public class AppTest
 {
-  public static readonly object[][] StringToDateCases =
+  public static readonly IEnumerable<object[]> StringToDateCases =
   [
-    ["26061965", new DateTime(1, 1, 1)],
-    ["08/05/1950", new DateTime(1950, 5, 8)],
-    ["", new DateTime(1, 1, 1)],
-    ["08-05-1950", new DateTime(1950, 5, 8)],
-    ["01-01-0001", new DateTime(1, 1, 1)],
+    ["26061965", new CultureInfo("en-GB"), new DateTime(1, 1, 1)],
+    ["08/05/1950", new CultureInfo("en-GB"), new DateTime(1950, 5, 8)],
+    ["08/05/1950", new CultureInfo("en-US"), new DateTime(1950, 8, 5)],
+    ["", new CultureInfo("en-GB"), new DateTime(1, 1, 1)],
+    ["08-05-1950", new CultureInfo("en-GB"), new DateTime(1950, 5, 8)],
+    ["08-05-1950", new CultureInfo("en-US"), new DateTime(1950, 8, 5)],
+    ["18-05-1950", new CultureInfo("en-GB"), new DateTime(1950, 5, 18)],
+    ["18-05-1950", new CultureInfo("en-US"), new DateTime(1, 1, 1)],
+    ["01-01-0001", new CultureInfo("en-GB"), new DateTime(1, 1, 1)],
   ];
 
   [Theory, MemberData(nameof(StringToDateCases))]
-  public void StringToDate(string source, DateTime expected)
+  public void StringToDate(string source, CultureInfo cultureInfo, DateTime expected)
   {
+    // Arrange
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    // Act
     DateTime result = App.StringToDate(source);
-    Assert.Equal(result, expected);
+    // Assert
+    Assert.Equal(expected, result);
   }
 
   public static readonly object[][] DateToStringCases =
   [
-    [null!, string.Empty],
-    [new DateTime(1950, 5, 8), "08.05.1950"],
+    [null!, new CultureInfo("en-GB"), string.Empty],
+    [new DateTime(1950, 5, 8), new CultureInfo("en-US"), "5/8/1950"],
+    [new DateTime(1950, 5, 8), new CultureInfo("en-GB"), "08/05/1950"],
+    [new DateTime(1950, 5, 8), new CultureInfo("es-ES"), "8/5/1950"],
+    [new DateTime(1950, 5, 8), new CultureInfo("de-DE"), "08.05.1950"],
+    [new DateTime(1950, 5, 8), new CultureInfo("it-IT"), "08/05/1950"],
   ];
 
   [Theory, MemberData(nameof(DateToStringCases))]
-  public void DateToString(DateTime? date, string expected)
+  public void DateToString(DateTime? date, CultureInfo cultureInfo, string expected)
   {
+    // Arrange
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    // Act
     string result = App.DateToString(date);
-    Assert.Equal(result, expected);
+    // Assert
+    Assert.Equal(expected, result);
   }
 }
