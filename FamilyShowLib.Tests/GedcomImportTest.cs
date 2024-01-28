4c56c00040a87ea00fb7d6286f0e6a5db18e5b0b
Jörg Hoffmann <Joerg.Hoffmann.GitHub@Web.de>
2024-01-28 17:42:26 +0100

    Add GedcomImport test

---
+++ FamilyShowLib.Tests/GedcomImportTest.cs
@@ -0,0 +1,22 @@
+﻿namespace FamilyShowLib.Tests;
+
+public class GedcomImportTest
+{
+  [StaFact]
+  public void Import()
+  {
+    GedcomImport sut = new();
+    PeopleCollection family = [];
+    SourceCollection source = [];
+    RepositoryCollection repository = [];
+    string location = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)!;
+    string fileName = Path.Combine(location, "Sample Files", "Kennedy.ged");
+    bool loaded = sut.Import(family, source, repository, fileName, true);
+
+    Assert.NotNull(sut);
+    Assert.True(loaded);
+    Assert.Equal(69, family.Count);
+    Assert.Equal(11, source.Count);
+    Assert.Empty(repository);
+  }
+}
