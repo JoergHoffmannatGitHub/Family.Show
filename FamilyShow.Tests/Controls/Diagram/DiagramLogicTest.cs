30201546608e6c1c4406d5862ccc8b498ada11d8
Jörg Hoffmann <Joerg.Hoffmann.GitHub@Web.de>
2025-01-12 10:59:49 +0100

    Adding comment how to test for refactoring.

---
+++ FamilyShow.Tests/Controls/Diagram/DiagramLogicTest.cs
@@ -0,0 +1,13 @@
+﻿namespace FamilyShow.Tests.Controls.Diagram;
+
+public class DiagramLogicTest
+{
+  // I have not found a way to set up DiagramLogic so that I can fill
+  // MinimumYear.get with values for PersonLookup and Connections. Until then,
+  // MinimumYear.get has to be tested on the complete system by loading
+  // different data sets. This ensures that each path is run through at least
+  // once.
+  // The following data records provide the following result:
+  // Windsor.familyx	1841
+  // Kennedy.ged		  1821
+}
